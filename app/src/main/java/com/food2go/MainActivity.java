4c1feaf82399831298502486a39d5f730d771dc9
marktrr <thm2802@gmail.com>
2019-12-02 17:08:52 -0500

    add SignIn activivty

--- app/src/main/java/com/food2go/MainActivity.java
+++ app/src/main/java/com/food2go/MainActivity.java
@@ -1,6 +1,8 @@
 package com.food2go;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +25,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        if(v.getId() == R.id.btnSignIn) {
+            Intent signIn = new Intent(MainActivity.this, SignIn.class);
+            startActivity(signIn);
+        }
     }
 }
