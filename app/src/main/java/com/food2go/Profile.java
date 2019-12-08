349772af6b56bc4ffd30f2cb39fa6e79b139fb70
Nathan Hannon <nathan@hannon.me>
2019-12-08 15:42:01 -0500

    Added photo gallery and camera support

--- app/src/main/java/com/food2go/Profile.java
+++ app/src/main/java/com/food2go/Profile.java
@@ -65,35 +65,29 @@ public class Profile extends AppCompatActivity implements View.OnClickListener
         //Labels
         lblFirst = findViewById(R.id.first_name_label);
         lblLast = findViewById(R.id.last_name_label);
-        lblEmail = findViewById(R.id.email_label);
         lblPhone = findViewById(R.id.phone_label);
         lblPassword = findViewById(R.id.password_label);
 
         //Text Fields
         txtFirstName = findViewById(R.id.editFirstName);
         txtLastName = findViewById(R.id.editLastName);
-        txtEmail = findViewById(R.id.editEmail);
         txtPhone = findViewById(R.id.editPhone);
         txtPassword = findViewById(R.id.editPassword);
 
         txtFirstName.setEnabled(false);
         txtLastName.setEnabled(false);
-        txtEmail.setEnabled(false);
         txtPhone.setEnabled(false);
         txtPassword.setEnabled(false);
 
         //Buttons
         btnAvatar = findViewById(R.id.avatar);
         btnAvatar.setOnClickListener(this);
-        btnEditProfile = findViewById(R.id.editProfile);
-        btnEditProfile.setOnClickListener(this);
         btnSave = findViewById(R.id.save);
         btnSave.setOnClickListener(this);
     }
 
     private void SaveUser()
     {
-        db.child("Users").child("profile").child("email").setValue(txtEmail.getText().toString());
         db.child("Users").child("profile").child("first name").setValue(txtFirstName.getText().toString());
         db.child("Users").child("profile").child("last name").setValue(txtLastName.getText().toString());
         db.child("Users").child("profile").child("phone").setValue(Integer.parseInt(txtPhone.getText().toString()));
@@ -182,21 +176,8 @@ public class Profile extends AppCompatActivity implements View.OnClickListener
             {
                 PickFromGallery();
             }
-            case R.id.editProfile:
-            {
-                txtFirstName.setEnabled(true);
-                txtLastName.setEnabled(true);
-                txtEmail.setEnabled(true);
-                txtPhone.setEnabled(true);
-                txtPassword.setEnabled(true);
-            }
             case R.id.save:
             {
-                txtFirstName.setEnabled(false);
-                txtLastName.setEnabled(false);
-                txtEmail.setEnabled(false);
-                txtPhone.setEnabled(false);
-                txtPassword.setEnabled(false);
                 SaveUser();
                 Toast.makeText(Profile.this, "Saved!", Toast.LENGTH_SHORT).show();
             }
